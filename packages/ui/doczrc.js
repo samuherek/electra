c910f7321fe40eabb7299a50470de19352390cac
Sam Uherek <samuherekbiz@gmail.com>
2019-01-16 19:25:09 +0100

    setup the base ui package

---
+++ packages/ui/doczrc.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'electra',
+  description: 'electra styles',
+  themeConfig: {
+    colors: {
+      primary: 'black',
+    },
+  },
+  typescript: true,
+  // wrapper: 'docz/wrapper',
+};
